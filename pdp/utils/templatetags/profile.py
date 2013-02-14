12576afc00b5c924b7409fa171f6a6eddcde7728
Romain Porte <microjoe@mailoo.org>
2013-02-14 21:48:48 +0100

    Added profile templatetag filter (close #2)

---
+++ pdp/utils/templatetags/profile.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from django import template
+
+from pdp.member.models import Profile
+
+register = template.Library()
+
+@register.filter('profile')
+def profile(user):
+    profile = Profile.objects.get(user=user)
+    return profile
