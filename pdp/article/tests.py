3a3a00c9259b3cd727be678afff252ae9f1515b3
Aethec <solal.pirelli@gmail.com>
2013-02-15 00:21:07 +0100

    Changed article tests.

--- pdp/article/tests.py
+++ pdp/article/tests.py
@@ -1,29 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django_dynamic_fixture import G
-from datetime import date
+from pytz import utc
+from datetime import *
 from pdp.article.models import *
-
-""" List of assert methods on TestCase:
-assertEqual(a, b)	      a == b	 
-assertNotEqual(a, b)	    a != b	 
-assertTrue(x)	          bool(x) is True	 
-assertFalse(x)	          bool(x) is False	 
-assertIs(a, b)	          a is b
-assertIsNot(a, b)	      a is not b
-assertIsNone(x)	          x is None
-assertIsNotNone(x)	      x is not None
-assertIn(a, b)	          a in b
-assertNotIn(a, b)	        a not in b
-assertIsInstance(a, b)	    isinstance(a, b)
-assertNotIsInstance(a, b)	not isinstance(a, b)
-"""
 
 class ArticleTests(TestCase):
     def test_latest_articles_zero(self):
@@ -32,20 +11,18 @@ class ArticleTests(TestCase):
 
     def test_latest_articles_one(self):
         """ Tests that the latest articles work if there is only one. """
-        article = G(Article, pubdate=date(2000,1,1))
+        article = G(Article, pubdate=datetime(2000,1,1,tzinfo=utc))
         self.assertEqual(1, len(get_last_articles()))
         self.assertEqual(article, get_last_articles()[0])
 
-    def test_latest_articles_four(self):
-        """ Tests that the latest articles are sorted correctly"""
-        a1 = G(Article, pubdate=date(2000,1,1))
-        a2 = G(Article, pubdate=date(2001,1,1))
-        a3 = G(Article, pubdate=date(2002,1,1))
-        a4 = G(Article, pubdate=date(2003,1,1))
-
+    def test_latest_articles_many(self):
+        """ Tests that the latest articles work correctly"""
+        articles = []
+        for n in range(2000, 1900, -1):
+            a = G(Article, pubdate=datetime(n,1,1,tzinfo=utc))
+            articles.append(a)
+        
         last = get_last_articles()
 
-        self.assertNotIn(a1, last)
-        self.assertIn(a2, last)
-        self.assertIn(a3, last)
-        self.assertIn(a4, last)
+        for n, val in enumerate(last):
+            self.assertEqual(val, articles[n])
--- pdp/forum/forms.py
+++ pdp/forum/forms.py
@@ -3,9 +3,9 @@
 from django import forms
 
 class TopicForm(forms.Form):
-	title = forms.CharField(max_length=80)
-	subtitle = forms.CharField(max_length=255, required=False)
-	text = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(max_length=80)
+    subtitle = forms.CharField(max_length=255, required=False)
+    text = forms.CharField(widget=forms.Textarea)
 
 class PostForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
