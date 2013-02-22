f2da799f94b5e121c1ed580e1ed3bf83d4e4127d
Romain Porte <microjoe@mailoo.org>
2013-02-22 15:18:20 +0100

    Written tests for profile templatetag

---
+++ pdp/utils/tests.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from django.test import TestCase
+from django_dynamic_fixture import G
+
+from django.contrib.auth.models import User
+
+from pdp.member.models import Profile
+from pdp.utils.templatetags.profile import profile
+
+
+class TemplateTagsTests(TestCase):
+    def test_profile_none(self):
+        '''Test the output of profile templatetag if profile does not exist'''
+        user = G(User)
+        self.assertEqual(None, profile(user))
+
+    def test_profile_existing(self):
+        '''Test the output of profile templatetag if profile does exist'''
+        user = G(User)
+        p = G(Profile, user=user)
+        self.assertEqual(p, profile(user))
