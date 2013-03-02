fea7f217cc32aeee6f95da86523d666418fe3664
Romain Porte <microjoe@mailoo.org>
2013-03-02 16:24:06 +0100

    Improved markdown helper

--- assets/js/pdp.js
+++ assets/js/pdp.js
@@ -3,15 +3,24 @@ $(document).ready(function() {
 
     $("textarea").before("<a href=\"/pages/aide-markdown\" class=\"btn btn-link mdhelper\">Aide-mémoire markdown</a>");
 
-    $(".mdhelper").click(
-        function(event) {
-            event.preventDefault();
+    $(".mdhelper").click(function(event) {
+        event.preventDefault();
+        var toggle = ($(this).data('toggle') == undefined) ? true : $(this).data('toggle');
+
+        if(toggle) {
             $.ajax({
                 url: "/pages/aide-markdown.ajax",
                 context: this
             }).done(function(msg){
                 $(this).before(msg);
-                $(this).remove();
+                $(this).text("Masquer");
             });
+        } else {
+            $(this).prev("table").remove();
+            $(this).text("Aide-mémoire markdown");
+        }
+
+        $(this).data("toggle", !toggle);
+
         });
 });
