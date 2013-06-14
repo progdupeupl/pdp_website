3fa35be44e339994f6b3e6adf5ba7048f53093f8
poulp <mathieu.nerv@gmail.com>
2013-06-14 11:02:04 +0200

    update parameter with 2 form : profile and account

--- pdp/member/forms.py
+++ pdp/member/forms.py
@@ -3,12 +3,15 @@
 from django import forms
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 from crispy_forms.helper import FormHelper
 from crispy_forms_foundation.layout import Layout, Div, Fieldset, Submit, Field, HTML
 from crispy_forms.bootstrap import FormActions
 
 from captcha.fields import CaptchaField
+
+from pdp.member.models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30)
@@ -76,6 +79,88 @@ class RegisterForm(forms.Form):
 
 
 class ProfileForm(forms.Form):
-    biography = forms.CharField(required=False, widget=forms.Textarea)
-    site = forms.CharField(required=False, max_length=128)
-    show_email = forms.BooleanField(required=False)
+    biography = forms.CharField(label='Biographie',required=False, widget=forms.Textarea(attrs={'placeholder':'Votre biographie au format Markdown.'}))
+    site = forms.CharField(label='Site Internet',required=False, max_length=128,widget=forms.TextInput(attrs={'placeholder':'Lien vers votre site internet personnel.'}),)
+    show_email = forms.BooleanField(label='Afficher mon adresse mail publiquement',required=False)
+
+    def __init__(self, user, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+
+        self.user = user
+        profile = Profile.objects.get(user=self.user)
+
+        self.helper.layout = Layout(
+            Fieldset(
+                u'Public',
+                Field('biography'),
+                Field('site'),
+                Field('show_email'),
+            ),
+            Div(
+                Submit('submit', 'Editer mon profil'),
+                css_class='button-group'
+            )
+        )
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+
+class ChangePasswordForm(forms.Form):
+    password_new = forms.CharField(label='Nouveau mot de passe ', max_length=76, widget=forms.PasswordInput)
+    password_old = forms.CharField(label='Mot de passe actuel ', max_length=76, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Confirmer le nouveau mot de passe ', max_length=76, widget=forms.PasswordInput)
+
+    def __init__(self, user, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+
+        self.user = user
+
+        self.helper.layout = Layout(
+            Fieldset(
+                u'Mot de passe',
+                Field('password_old'),
+                Field('password_new'),
+                Field('password_confirm'),
+            ),
+            Div(
+                Submit('submit', 'Changer mon mot de passe'),
+                css_class='button-group'
+            )
+        )
+        super(ChangePasswordForm, self).__init__(*args, **kwargs)
+
+
+    def clean(self):
+        cleaned_data = super(ChangePasswordForm, self).clean()
+
+        password_old = cleaned_data.get('password_old')
+        password_new = cleaned_data.get('password_new')
+        password_confirm = cleaned_data.get('password_confirm')
+   
+        
+        # check if the actual password is correct 
+        if password_old:
+            user_exist = authenticate(username=self.user.username, password=password_old)
+            # bad password go sodomize yourself
+            if not user_exist and password_old != "":
+                self._errors['password_old'] = self.error_class([u'Mot de passe incorrect'])
+                if 'password_old' in cleaned_data:
+                    del cleaned_data['password_old']
+
+        # Check that the password and it's confirmation match
+        if not password_confirm == password_new:
+            msg = u'Les mots de passe sont différents'
+            self._errors['password_new'] = self.error_class([''])
+            self._errors['password_confirm'] = self.error_class([msg])
+
+            if 'password_new' in cleaned_data:
+                del cleaned_data['password_new']
+
+            if 'password_confirm' in cleaned_data:
+                del cleaned_data['password_confirm']
+
+        return cleaned_data
+
--- pdp/member/views.py
+++ pdp/member/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render_to_response
 from django.http import Http404
 
 from django.contrib.auth.models import User, SiteProfileNotAvailable
@@ -8,12 +8,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
 from django.core.context_processors import csrf
+from django.template import RequestContext
 
 from pdp.utils.tokens import generate_token
 from pdp.utils import render_template
 
 from models import Profile
-from forms import LoginForm, ProfileForm, RegisterForm
+from forms import LoginForm, ProfileForm, RegisterForm, ChangePasswordForm
 
 from pdp.forum.models import Forum
 
@@ -127,3 +128,58 @@ def register_view(request):
     return render_template('member/register.html', {
         'form': form
     })
+
+@login_required
+def settings_profile(request):
+    #extra information about the current user
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.user, request.POST)
+        c = {
+            'form' : form,
+        }
+        if form.is_valid():
+            profile.biography = form.data['biography']
+            profile.site = form.data['site']
+            profile.show_email = 'show_email' in form.data 
+
+            # Save the profile
+            try:
+                profile.save()
+            except:
+                raise Http404
+
+            return redirect(profile.get_absolute_url())
+        else:
+            return render_to_response('member/settings_profile.html',c,RequestContext(request))
+    else:
+        form = ProfileForm(request.user,initial={
+            'biography':profile.biography,
+            'site':profile.site,
+            'show_email':profile.show_email}
+        )
+        c = {
+            'form' : form
+        }
+        return render_to_response('member/settings_profile.html',c,RequestContext(request))
+
+@login_required
+def settings_account(request):
+    if request.method == 'POST':
+        form = ChangePasswordForm(request.user,request.POST)
+        c = {
+            'form' : form,
+        }
+        if form.is_valid():
+            request.user.set_password(form.data['password_new'])
+            request.user.save()
+            return redirect(request.user.get_absolute_url())
+        else:
+            return render_to_response('member/settings_account.html',c,RequestContext(request))
+    else:
+        form = ChangePasswordForm(request.user)
+        c = {
+            'form' : form,
+        }
+        return render_to_response('member/settings_account.html',c,RequestContext(request))
