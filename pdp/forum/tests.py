1f2b641ec17e8c2a7db04d2bc23184ecee14c71b
Romain Porte <microjoe@mailoo.org>
2013-06-19 00:44:25 +0200

    Added some tests for member app

--- pdp/forum/tests.py
+++ pdp/forum/tests.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 
 from django.test import TestCase
-from django_dynamic_fixture import G
-
 from django.contrib.auth.models import User
+
+from django_dynamic_fixture import G
 
 from .models import Category, Forum, Topic, Post
 
--- pdp/member/tests.py
+++ pdp/member/tests.py
@@ -1,9 +1,30 @@
 # coding: utf-8
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+from django_dynamic_fixture import G
+
 from models import Profile
 
 class SimpleTest(TestCase):
     def test_index(self):
-        resp = self.client.get('/membres/')
+        resp = self.client.get(reverse('pdp.member.views.index'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_details(self):
+        user = G(User, username='toto')
+        profile = G(Profile, user=user)
+
+        resp = self.client.get(reverse('pdp.member.views.details',
+                               args=[user.username]))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_register(self):
+        resp = self.client.get(reverse('pdp.member.views.register_view'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_login(self):
+        resp = self.client.get(reverse('pdp.member.views.login_view'))
         self.assertEqual(resp.status_code, 200)
