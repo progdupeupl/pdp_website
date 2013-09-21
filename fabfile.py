d7efe965e560c03c03fc3384d7fb127d5f93f5da
Romain Porte <microjoe@mailoo.org>
2013-09-21 18:12:36 +0200

    Added search utils in fabfile

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,13 @@ def test():
     local('python manage.py test {0}'.format(' '.join(TEST_APPS)))
 
 
+def initsearch():
+    local('python manage.py rebuild_index')
+
+
+def updatesearch():
+    local('python manage.py update_index')
+
 try:
     from fabfile_local import *
 except ImportError:
