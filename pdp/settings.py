0d7d0ae1ef7f16f3a204b3a6d2fd9fe1f5911c2e
Romain Porte <microjoe@mailoo.org>
2013-09-21 14:36:36 +0200

    Removed user added path from previous PR

--- pdp/settings.py
+++ pdp/settings.py
@@ -81,7 +81,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(SITE_ROOT, 'assets'),
-    "/home/willy/pyproject/pdp/progdupeupl/assets",
 )
 
 # List of finder classes that know how to find static files in
