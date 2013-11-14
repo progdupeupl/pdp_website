d969e556322040a5e6640d45079ceb8862f044f7
Romain Porte <microjoe@mailoo.org>
2013-11-14 19:09:02 +0100

    Not using handmade forms for messages

--- pdp/messages/forms.py
+++ pdp/messages/forms.py
@@ -2,12 +2,40 @@
 
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms_foundation.layout import Layout, Submit, Field
+
 
 class PrivateTopicForm(forms.Form):
-    participants = forms.CharField()
-    title = forms.CharField(max_length=80)
-    subtitle = forms.CharField(max_length=255, required=False)
-    text = forms.CharField(widget=forms.Textarea)
+    participants = forms.CharField(
+        label=u'Participants (séparés par une virgule)',
+        required=True)
+
+    title = forms.CharField(
+        label=u'Titre', required=True,
+        max_length=80)
+
+    subtitle = forms.CharField(
+        label=u'Sous-titre',
+        max_length=255, required=False)
+
+    text = forms.CharField(
+        label=u'Texte',
+        widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Field('participants'),
+            Field('title'),
+            Field('subtitle'),
+            Field('text'),
+            Submit('submit', u'Envoyer'),
+            Submit('preview', u'Prévisualisation'),
+        )
+        super(PrivateTopicForm, self).__init__(*args, **kwargs)
 
 
 class PrivatePostForm(forms.Form):
