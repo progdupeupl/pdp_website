92b4aa67715ca0ab3c8c6809769cf67530e54b8b
Romain Porte <microjoe@mailoo.org>
2014-01-05 01:03:30 +0100

    Added more tests for gallery app

--- pdp/gallery/tests.py
+++ pdp/gallery/tests.py
@@ -4,12 +4,51 @@
 
 from django.test import TestCase
 
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+from django_dynamic_fixture import G
+
+from pdp.member.models import Profile
+
 
 class GalleryIntegrationTests(TestCase):
 
     """Integration tests with no specific valid instances."""
 
-    def test_index_anon(self):
+    def test_url_index(self):
         """Test to view gallery index as anonymous user."""
-        resp = self.client.get('/galerie/')
+        resp = self.client.get(reverse('pdp.gallery.views.gallery_list'))
         self.assertEquals(302, resp.status_code)
+
+    def test_url_new(self):
+        """Test to create new gallery index as anonymous user."""
+        resp = self.client.get(reverse('pdp.gallery.views.new_gallery'))
+        self.assertEquals(302, resp.status_code)
+
+
+class AuthenticatedGalleryIntegrationTests(TestCase):
+
+    """Integration tests with logged-in user."""
+
+    def setUp(self):
+        # Create user
+        self.user = G(User, username='test')
+        self.user.set_password('test')
+        self.user.save()
+
+        # Create profile
+        self.profile = G(Profile, user=self.user)
+
+        # Authenticate user
+        self.client.login(username='test', password='test')
+
+    def test_url_index(self):
+        """Test to view gallery index as logged-in user."""
+        resp = self.client.get(reverse('pdp.gallery.views.gallery_list'))
+        self.assertEquals(200, resp.status_code)
+
+    def test_url_new(self):
+        """Test to create new gallery index as logged-in user."""
+        resp = self.client.get(reverse('pdp.gallery.views.new_gallery'))
+        self.assertEquals(200, resp.status_code)
