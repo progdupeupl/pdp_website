c6d72863b99ce331abd1e8f71939328d9d44e36f
Romain Porte <microjoe@mailoo.org>
2014-01-06 14:21:21 +0100

    Fixed bugs in gallery forms

--- pdp/gallery/forms.py
+++ pdp/gallery/forms.py
@@ -7,23 +7,23 @@ from django.conf import settings
 
 
 class UserGalleryForm(forms.Form):
-    user= forms.CharField('Membre', required=False)
-    gallery = forms.CharField('Gallery', required=False)
-    mode = forms.CharField('Mode', required=False)
+    user = forms.CharField(label=u'Membre', required=False)
+    gallery = forms.CharField(label=u'Gallerie', required=False)
+    mode = forms.CharField(label=u'Mode', required=False)
 
 
 class GalleryForm(forms.Form):
-    title= forms.CharField(max_length=80)
+    title = forms.CharField(max_length=80)
     subtitle = forms.CharField(max_length=200, required=False)
 
 
 class ImageForm(forms.Form):
-    gallery = forms.CharField('Gallery', required=False)
+    gallery = forms.CharField(label=u'Gallery', required=False)
 
     physical = forms.ImageField(
         label='Select an image',
         help_text=u'max. {} megabytes'.format(settings.IMAGE_MAX_SIZE),
         required=False)
 
-    title = forms.CharField('Titre')
-    legend = forms.CharField('Legende', required=False)
+    title = forms.CharField(label=u'Titre')
+    legend = forms.CharField(label=u'Légende', required=False)
