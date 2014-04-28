36332e0a73cacc918f8d9f43056d5773ee373738
Romain Porte <microjoe@mailoo.org>
2014-04-28 16:09:43 +0200

    Handling redirection on connect button

--- pdp/settings.py
+++ pdp/settings.py
@@ -192,6 +192,7 @@ TEMPLATE_DIRS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     # Default context processors
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
