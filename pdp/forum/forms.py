ae95383ab76055f82952a88702a38c6caf7449f9
Romain Porte <microjoe@mailoo.org>
2014-06-11 22:37:18 +0200

    Using crispy for new topics (close #149)

--- pdp/forum/forms.py
+++ pdp/forum/forms.py
@@ -17,11 +17,51 @@
 
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div
+
+from crispy_forms_foundation.layout import Layout, Submit, HTML, Field
+
 
 class TopicForm(forms.Form):
-    title = forms.CharField(max_length=80)
-    subtitle = forms.CharField(max_length=255, required=False)
-    text = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(
+        label=u'Titre',
+        max_length=80,
+        widget=forms.TextInput(attrs={'autofocus': 'autofocus'})
+    )
+
+    subtitle = forms.CharField(
+        label=u'Sous-titre',
+        max_length=255,
+        required=False
+    )
+
+    text = forms.CharField(
+        label=u'Texte',
+        widget=forms.Textarea
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Field('title'),
+            Field('subtitle',
+                  placeholder=u'Facultatif, permet de mieux décrire votre '
+                              u'sujet.'),
+            HTML('{% include "misc/editor.part.html" %}'),
+            Field('text',
+                  placeholder=u'Contenu de votre message au format Markdown.'),
+            Div(
+                Submit('submit', u'Créer le sujet'),
+                Submit('preview', u'Prévisualiser', css_class='secondary'),
+                HTML(u'<a href="{{ forum.get_absolute_url }}" class="button '
+                     u'secondary">Retour</a>'),
+                css_class='button-center'
+            )
+        )
+        super(TopicForm, self).__init__(*args, **kwargs)
 
 
 class PostForm(forms.Form):
