c74baecd3dde87471ca97101e6027cfdd5f42abd
Romain Porte <microjoe@mailoo.org>
2014-06-23 20:29:19 +0200

    CNIL cleanup

--- templates/cnil.part.js
+++ templates/cnil.part.js
@@ -1,39 +1,34 @@
-// Remplacez la valeur UA-XXXXXX-Y par l'identifiant analytics de votre site.
 gaProperty = '{{ key }}'
 
-// Désactive le tracking si le cookie d’Opt-out existe déjà.
 var disableStr = 'ga-disable-' + gaProperty;
 
 if (document.cookie.indexOf('hasConsent=false') > -1) {
     window[disableStr] = true;
 }
 
-// Cette fonction retourne la date d’expiration du cookie de consentement 
 function getCookieExpireDate() { 
-    var cookieTimeout = 34214400000;// Le nombre de millisecondes que font 13 mois
+    var cookieTimeout = 34214400000; // 13 months
     var date = new Date();
     date.setTime(date.getTime()+cookieTimeout);
     var expires = "; expires="+date.toGMTString();
     return expires;
 }
 
-// Cette fonction est appelée pour afficher la demande de consentement
 function askConsent(){
     var bodytag = document.getElementsByTagName('header')[0];
     var div = document.createElement('div');
     div.setAttribute('id','cookie-banner');
-    // Le code HTML de la demande de consentement
-    // Vous pouvez modifier le contenu ainsi que le style
-    div.innerHTML =  '<p>Ce site utilise Google Analytics.\
+
+    div.innerHTML =  '<p> \
     En continuant à naviguer, vous nous autorisez à déposer des cookies à des fins de \
     mesure d’audience. \
-    <a href="javascript:gaOptout()" class="button mini">Non merci</a> \
+    <a href="javascript:gaOk()" class="button mini success">Ok</a> \
+    <a href="{% url "pdp.pages.views.tos" %}#cookies" class="button mini">En savoir plus</a> \
     </p>';
-    bodytag.insertBefore(div,bodytag.firstChild); // Ajoute la bannière juste au début de la page 
-    document.getElementsByTagName('body')[0].className+=' cookiebanner';
+
+    bodytag.insertBefore(div,bodytag.firstChild);
 }
 
-// Retourne la chaine de caractère correspondant à nom=valeur
 function getCookie(NomDuCookie)  {
     if (document.cookie.length > 0) {
         begin = document.cookie.indexOf(NomDuCookie+"=");
@@ -47,7 +42,6 @@ function getCookie(NomDuCookie)  {
     return null;
 }
 
-// Fonction d'effacement des cookies   
 function delCookie(name )   {
     path = ";path=" + "/";
     domain = ";domain=" + "."+document.location.hostname;
@@ -55,42 +49,37 @@ function delCookie(name )   {
     document.cookie = name + "=" + path + domain + ";expires=" + expiration;
 }
 
-// Efface tous les types de cookies utilisés par Google Analytics
 function deleteAnalyticsCookies() {
     var cookieNames = ["__utma","__utmb","__utmc","__utmz","_ga"]
     for (var i=0; i<cookieNames.length; i++)
         delCookie(cookieNames[i])
 }
 
-// La fonction d'opt-out
 function gaOptout() {
     document.cookie = disableStr + '=true;'+ getCookieExpireDate() +' ; path=/';
     document.cookie = 'hasConsent=false;'+ getCookieExpireDate() +' ; path=/';
 
-    var div = document.getElementById('cookie-banner');
-
-    if ( div!= null ) div.innerHTML = '<p> Vous vous êtes opposé \
-    Au dépôt de cookies de mesures d\'audience dans votre navigateur.</p>'
+    alert('Vous vous êtes opposé au dépôt de cookies de mesures d’audience dans votre navigateur.');
 
     window[disableStr] = true;
     deleteAnalyticsCookies();
 }
 
-// Ce bout de code vérifie que le consentement n'a pas déjà été obtenu avant d'afficher
-// la baniére
+function gaOk() {
+    document.cookie = 'hasConsent=true; '+ getCookieExpireDate() +' ; path=/'; 
+
+    var div = document.getElementById('cookie-banner');
+    if (div != null) div.style.visibility = 'hidden'
+}
+
 var consentCookie =  getCookie('hasConsent');
 if (!consentCookie) {
- //L'utilisateur n'a pas encore de cookie de consentement
  var referrer_host = document.referrer.split('/')[2]; 
    if ( referrer_host != document.location.hostname ) {
-     // si il vient d'un autre site on désactive le tracking et on affiche la
-     // demande de consentement
      window[disableStr] = true;
      window[disableStr] = true;
      window.onload = askConsent;
-   } else { //sinon on lui dépose un cookie 
+   } else {
       document.cookie = 'hasConsent=true; '+ getCookieExpireDate() +' ; path=/'; 
    }
 }
-
-// Il vous reste à insérer votre tag javascript ici   
