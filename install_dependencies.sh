d9a998f079c54be3f27c78174cc0612f994d4eac
Romain Porte <microjoe@mailoo.org>
2015-04-28 15:47:47 +0200

    Improved dependencies installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
+# Note: we must use Bash over POSIX Shell here because of the "source" command
+# allowing use to switch inside the virtualenv once created.
+#
+# This is the only reason why bash is kept here instead of sh. Rest of the
+# script should be POSIX compatible.
+
 # Check for Rubygems
 gempath=$(which gem)
 if [ ! -f "$gempath" ]; then
 	echo "You need to install the Ruby `gem` package manager"
+	exit 1
 fi
 
-# Check for Python virtualenv
+# Check for Python virtualenv tool
 venvpath=$(which virtualenv)
 if [ ! -f "$venvpath" ]; then
 	echo "You need to install Python 3 virtualenv tool"
+	exit 2
+fi
+
+# Check if the C compiler is GCC
+#
+# This check is performed because the C Python packages this project rely on
+# will fail to compile using the well-known Clang compiler ATM.
+if [ "$CC" != "gcc" ]; then
+	echo "You must use GCC to install the Python dependencies, please fix" \
+		"your \$CC environment variable."
+	echo "Current \$CC value is : $CC"
+	exit 3
 fi
 
 # Install gems
@@ -25,11 +44,4 @@ source venv/bin/activate
 
 # Installing Python dependencies in virtualenv
 echo "Installing Python dependencies inside the virtualenv..."
-
-if [ "$CC" != "gcc" ]; then
-	echo "You must use GCC to install the Python dependencies, please fix" \
-		"your \$CC environment variable."
-	exit 1
-fi
-
 pip install -r requirements.txt && echo "Successfully installed PDP dependencies!"
